936791bbf7a51e3c989a675fc285bbd8b1c16b56
Taigaueoka <noi0211coinlaundry@gmail.com>
2019-10-26 21:04:33 +0000

    ‘最初のコミット’

---
+++ app/models/task.rb
@@ -0,0 +1,29 @@
+class Task < ApplicationRecord
+
+  has_one_attached :image
+  
+  def self. ransackable_attributes(auth_object = nil)
+    %w[name created_at]
+  end
+  
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
+  validates :name, presence: true, length: { maximum: 30 }
+  validate :validate_name_not_including_comma
+
+  belongs_to :user
+
+  scope :recent, -> { order(created_at: :desc) }
+
+  private
+
+
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
+  
+  def set_nameless_name
+    self.name = '名前なし' if name.blank?
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+
+  get 'posts/index'
+  get 'posts/show'
+  get 'users/index'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  namespace :admin do
+    resources :users
+  end
+
+  root to: 'tasks#index'
+  resources :tasks do
+    post :confirm, action: :confirm_new, on: :new
+  end
+  
+  root to: 'profiles#index'
+  resources :profiles do
+    post :confirm, action: :confirm_new, on: :new
+  end
+end
