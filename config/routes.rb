828b7ef1aec878a1d9aa7feb397cbde25653b83a
Taigaueoka <noi0211coinlaundry@gmail.com>
2019-10-30 21:50:57 +0000

    S3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'topics/index'
   get 'topics/show/:id' => 'topics#show', as: :topics_show
-  delete 'topics/delete/:id' => 'topics#delete', as: :topic_delete
+  delete 'topics/:id' => 'topics#delete', as: :topic_delete
   post 'topics/create' => 'topics#create'
   post 'posts/create' => 'post#create', as: :post_create
   get 'posts/index'
